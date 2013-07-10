c3f8f90110a711fad5fc8be5baaef05165bddefc
Niklas Callsen <niklas@entless.org>
2013-07-10 12:13:10 +0000

    Design Update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 
+$(document).ready(function() {
+	logoTransition();
+});
+
+// Scrolling Header effects (logo + profile)
+function logoTransition() {
+	$(document).scroll(function() {
+		var nav = $("#nav");
+		var logoText = $("#tradity");
+		var nameContainer = $(".name-container");
+
+		if ($(window).scrollTop() > 130) {
+			logoText.slideDown(300);
+			nav.css({"position" : "fixed"});
+			nav.animate({"top" : "50px"}, 550);
+			nameContainer.addClass("fixed-name-container");
+			nameContainer.slideDown(300);
+		}
+		else {
+			logoText.slideUp(100);
+			nav.css({
+					"position" : "static",
+					"top" : "auto" //warum dusselt er damit noch rum, nachdem man hochscrollt?
+					});
+			nameContainer.removeClass("fixed-name-container");	
+		}
+	});
+}

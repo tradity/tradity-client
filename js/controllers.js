3ee448b78c017a5f9c0c6c3ec4d8c013d0045959
Nico Haesler <nico@entless.org>
2013-07-25 22:41:35 +0000

    Urlaubscommit

--- js/controllers.js
+++ js/controllers.js
@@ -1,15 +1,35 @@
 'use strict';
 
 angular.module('tradity.controllers', []).
-  controller('LoginCtrl', function($scope, socket) {
-    socket.on('login', function(data) {
-      console.log('Möglichkeit 2', data);
-    });
+  controller('LoginCtrl', function($scope, $location, socket) {
     $scope.register = function() {
       socket.emit('login', {
         name: $scope.username,
         pw: $scope.password,
         stayloggedin: false
-      }, function(data) { console.log('Möglichkeit 1', data); });
+      },
+      function(data) {
+        switch (data.code) {
+          case 'login-success':
+            $location.path('/')
+            break;
+          case 'login-badname':
+            alert('Benutzer existiert nicht');
+            break;
+          case 'login-wrongpw':
+            alert('Falsches Passwort');
+            break;
+          case 'login-email-not-verified':
+            alert('Emailadresse noch nicht bestätigt');
+        }
+      });
     };
+  }).
+  controller('MainCtrl', function($scope, $location, socket) {
+    socket.on('response', function(data) {
+      if (data.code == 'not-logged-in') {
+        alert('Nicht eingeloggt');
+        $location.path('/login');
+      }
+    });
   });

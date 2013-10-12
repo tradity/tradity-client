1b25fa359c22e983bdac6bf4bcb5b8c7d6c097c0
Hauke Henningsen <sqrt@tradity.de>
2013-10-12 20:20:44 +0000

    Basic chart implementation

--- game/js/controllers.js
+++ game/js/controllers.js
@@ -653,7 +653,75 @@ angular.module('tradity.controllers', []).
             $scope.user.profilepic = $scope.serverConfig.defaultprofile;
           data.pinboard.sort(function(a,b) { return b.time - a.time; });
           $scope.comments = data.pinboard;
+          
+          $scope.draw();
         }
+      });
+    };
+    $scope.drawMode = 1;  
+    $scope.drawModes = [
+      {name: '24 h', days: 1,  marker: true},
+      {name: '48 h', days: 2,  marker: true},
+      {name: '7 d',  days: 7,  marker: false},
+      {name: '14 d', days: 14, marker: false},
+      {name: 'seit Beginn', days: 1/0, marker: false}
+    ];
+    $scope.curPlot = null;
+    $scope.draw = function(newMode) {    
+      if (newMode != null)
+        $scope.drawMode = newMode;
+      
+      var mode = $scope.drawModes[$scope.drawMode];
+      console.log(mode, $scope.drawMode);
+      var now = new Date().getTime();
+      var tmin = now, tmax = 0,vmin = 1/0,vmax=0;
+      var data = $.map($.grep($scope.values, function(e) {
+        return now/1000 - e.time < 86400 * mode.days;
+      }), function(e) {
+        if (e.time*1000 < tmin) tmin = e.time*1000;
+        if (e.time*1000 > tmax) tmax = e.time*1000;
+        if (e.value/10000 < vmin) vmin = e.value/10000;
+        if (e.value/10000 > vmax) vmax = e.value/10000;
+        return [[e.time*1000, e.value/10000]];
+      });
+      if ($scope.curPlot) $scope.curPlot.destroy();
+      $scope.curPlot = $.jqplot('chart', [data], {
+        title: 'Performance ' + mode.name,
+        axes: {
+          xaxis: {
+            renderer: $.jqplot.DateAxisRenderer,
+            min: tmin,
+            max: tmax,
+            tickRenderer: $.jqplot.CanvasAxisTickRenderer,
+            tickOptions: {
+              angle: -30,
+              formatString: '%d.%m, %R',
+            }
+          },
+          yaxis: {
+            min: vmin * 0.85,
+            max: vmax / 0.85,
+            tickOptions: {
+              formatString: '%.2f €'
+            }
+          }
+        },
+        highlighter: {
+          show: true,
+          sizeAdjust: 2
+        },
+        cursor: {
+          show: true,
+          zoom: true
+        },
+        shadow: false,
+        shadowAlpha: 1,
+        series: [
+          {
+            showMarker: mode.marker,
+            markerOptions: { style: 'circle', size: 2 }
+          }
+        ]
       });
     };
     $scope.addToWatchlist = function() {

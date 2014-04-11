59538906a58871f98941457046e87cb82d76ce48
Barne Jürgensen <barne@tradity.de>
2014-04-11 22:02:36 +0000

    fix herounit scroll

--- app/js/controllers/herounit.js
+++ app/js/controllers/herounit.js
@@ -9,7 +9,7 @@ angular.module('tradity').
 		};
 
 		$scope.goto = function (id) {
-			$("#hero-wrapper").animate({scrollTop:$('#'+id).offset().top}, 1700)
+			$("body").animate({scrollTop:$('#'+id).offset().top}, 1700)
 		}
 
 		socket.on('get-user-info', function(data) {

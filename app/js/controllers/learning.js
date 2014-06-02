2e75c54a089a1206e19492e7d50e859ed5ae90a4
Barne Jürgensen <barne@tradity.de>
2014-06-02 15:42:49 +0000

    learning

--- app/js/controllers/learning.js
+++ app/js/controllers/learning.js
@@ -3,22 +3,44 @@ angular.module('tradity').
 		$scope.learningQuestions = [
 			{
 				katalog:0,
-				name:'Grundlage',
-				description:'bin ich toll ?',
+				description:'Aus welchen drei Komponenten besteht das sogenannte "Magische Dreieck" bei Investments?',
 				answers:{
-					0:'ja',
-					1:'nein',
+					0:'Preis, Leistung, Gebühren ',
+					1:'Rendite, Risiko und Verfügbarkeit ',
+					2:'Qualität, Marktlage, Nachrichten'
 				},
-				right:0
+				right:1
+			},
+			{
+				katalog:0,
+				description:'Welche Eigenschaften sind bei "grünen" Anlagen garantiert?',
+				answers:{
+					0:'CO² neutral und ohne Genpflanzen',
+					1:'Ausschließlich Verwendung nachwachsender Rohstoffe und Fairtraide ',
+					2:'keine - die Bezeichnung ist nicht rechtlich geschützt '
+				},
+				right:2
+			},
+			{
+				katalog:0,
+				description:'Was bedeutet das "Best-in-Class" Prinzip bei nachhaltigen Anlagen? ',
+				answers:{
+					0:'Es werden die klassenbesten Schüler in Entwicklungsländern mit einem Stipendium gefördert, damit sie weiter zur Schule gehen können.',
+					1:'Die Anbieter suchen Firmen aus, die in ihrer Branche in Sachen Umwelt- und/oder Sozialstandards eine Vorreiterrolle einnehmen. Keine Branche wird von vorneherein ausgeschlossen, auch Wirtschaftszweige wie die Atom- oder die Rüstungsindustrie können mit in einem Portfolio landen.',
+					2:'Es wird in die Sieger vom Öko-Siegel investiert.'
+				},
+				right:1
 			}
 		];
 
 		$scope.learaningKatalog = [
 			{
 				id:0,
-				name:'Grundlagen',
-				description:'Da da daaa',
+				name:'Grüne Geldanlage',
+				description:'Umwelt- und Klimaschutz fängt bei jedem zu hause an, das ist klar. Aber ein großer Teil der Emissionen geht von Unternehmen aus. Wie kann man mit Investments ethische, nachhaltige und grüne Unternehmen fördern? Zielgerichtete "grüne" Geldanlagen können hierbei eine wichtige Rolle spielen. Doch Achtung: Grün ist nicht gleich grün.',
 				requirements:[],
+				link:'https://boersenspiel.tradity.de/learning/gruene-geldanlage/',
+				show:false
 			}
 		];
 
@@ -36,6 +58,7 @@ angular.module('tradity').
 
 		$scope.prove = function() {
 			console.log($scope.questions)
+			$scope.proved = true;
 			for (var i = $scope.questions.length - 1; i >= 0; i--) {
 				if ($scope.questions[i].answer == $scope.questions[i].right)
 					$scope.questions[i].wrong = false;
---
+++ app/js/controllers/notifications.js
@@ -0,0 +1,41 @@
+angular.module('tradity').
+	controller('NotificationsCtrl', function($scope, $stateParams, $state, socket, $timeout) {
+		$scope.notifications = [];
+
+		$scope.add = function(notification) {
+			var index = $scope.notifications.push(notification);
+
+			if (!notification.sticky)
+				$timeout(function(){
+					$scope.notifications.splice(index-1,1);
+					console.log(index)
+					console.log($scope.notifications)
+				},5000);
+		};
+
+		
+
+		$scope.test = function() {
+			$scope.add({
+				message:'test nachricht app gestartet',
+				link:function() {
+
+				},
+				sticky:false,
+			})
+		}
+
+		$scope.close = function(index) {
+			$scope.notifications.splice(index,1);
+		}
+
+		$scope.test();
+
+		socket.on('comment', function(event) {
+			if (event.baseeventtype == 'chat-start') {
+				console.log('##push', event);
+			}
+		});
+
+
+	});

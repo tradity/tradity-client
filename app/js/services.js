2f04b8daeff1d9d5e35807616b829ebc98d3ebc4
Hauke Henningsen <sqrt@tradity.de>
2014-06-08 02:58:18 +0000

    meow

--- app/js/services.js
+++ app/js/services.js
@@ -29,7 +29,7 @@ angular.module('tradity').
 			},
 			hasOpenQueries: function () { return socket.hasOpenQueries(); },
 			txPackets: function() { return socket.txPackets(); },
-			rxPackets: function() { return socket.rxPackets(); }
+			rxPackets: function() { return socket.rxPackets(); },
 			reconnect: function() { return socket.reconnect(); }
 		};
 	});

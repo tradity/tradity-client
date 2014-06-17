e8d55591c331d294be8217769c19659d537dbdb2
Hauke Henningsen <sqrt@tradity.de>
2014-06-17 16:31:00 +0000

    fix

--- app/js/controllers/unknownEntity.js
+++ app/js/controllers/unknownEntity.js
@@ -4,10 +4,8 @@ angular.module('tradity').
 		if (!entity)
 			$state.go('game.feed');
 		
-		console.log(entity);
 		if (entity && entity.toString().toLowerCase() == '/bad-oldesloe')
 			entity = '/BadOldesloe';
-		console.log(entity);
 		
 		socket.emit('school-exists', {
 			lookfor: entity,

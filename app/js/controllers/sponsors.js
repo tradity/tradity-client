e6e64fd284fb43fd1d523a6f2764bb8031255be8
Hauke Henningsen <sqrt@tradity.de>
2014-06-18 11:26:11 +0000

    fix

--- app/js/controllers/sponsors.js
+++ app/js/controllers/sponsors.js
@@ -3,46 +3,57 @@ angular.module('tradity').
 		$scope.sponsors = [
 			{
 				picture: 'https://boersenspiel.tradity.de/wp-content/uploads/2014/05/Tradity_Boersenspiel_Flatex.jpg',
-				link: 'https://www.flatex.de/'
+				link: 'https://www.flatex.de/',
+				group: true
 			},
 			{
 				picture: 'https://boersenspiel.tradity.de/wp-content/uploads/2014/05/Logo_Logitech.png',
-				link: 'http://www.logitech.com/de-de/speakers-audio/home-pc-speakers'
+				link: 'http://www.logitech.com/de-de/speakers-audio/home-pc-speakers',
+				group: true,
 			},
 			{
 				picture: 'https://boersenspiel.tradity.de/wp-content/uploads/2014/05/Logo_Boerse_Frankfurt.png',
-				link: 'http://www.boerse-frankfurt.de/de/start#&reiter=vedesanleihe'
+				link: 'http://www.boerse-frankfurt.de/de/start#&reiter=vedesanleihe',
+				group: true,
 			},
 			{
 				picture: 'https://boersenspiel.tradity.de/wp-content/uploads/2014/06/Logo_Campello.png',
-				link: 'http://www.campello-store.com/'
+				link: 'http://www.campello-store.com/',
+				group: true,
 			},
 			{
 				picture: 'https://boersenspiel.tradity.de/wp-content/uploads/2014/06/Logo_VRBankNiebuell.png',
 				link: 'https://www.vrbankniebuell.de/privatkunden.html',
-				schoolPathRegex: /^\/niebuell(\/|$)/i
+				schoolPathRegex: /^\/niebuell(\/|$)/i,
+				group: true
 			},
 			{
 				picture: 'https://boersenspiel.tradity.de/wp-content/uploads/2014/06/Logo_VRStormarn.png',
 				link: 'https://www.volksbank-stormarn.de/privatkunden.html',
-				schoolPathRegex: /^\/BadOldesloe(\/|$)/i
+				schoolPathRegex: /^\/BadOldesloe(\/|$)/i,
+				group: true
 			},
 			{
 				picture: 'https://boersenspiel.tradity.de/wp-content/uploads/2014/06/Logo_VRBANKHUSUM.png',
 				link: 'https://www.husumer-volksbank.de/homepage.html',
-				schoolPathRegex: /^\/Husum(\/|$)/i
+				schoolPathRegex: /^\/Husum(\/|$)/i,
+				group: true
 			},
 		]
+		
+		$scope.group = null;
 
 		$scope.getShown = function (data) {
+			$scope.group = $scope.$parent && $scope.$parent.$parent && $scope.$parent.$parent.school;
+		
 			data = data || $rootScope.ownUser;
 			
 			if (!data)
 				return;
 
-			var userSchoolPath = (data.bottomLevelSchool || {path: '/'}).path;
+			var userSchoolPath = $scope.group ? $scope.group.path : (data.bottomLevelSchool || {path: '/'}).path;
 			for (var i = $scope.sponsors.length - 1; i >= 0; i--)
-				$scope.sponsors[i].show = !$scope.sponsors[i].schoolPathRegex || $scope.sponsors[i].schoolPathRegex.test(userSchoolPath);
+				$scope.sponsors[i].show = (!$scope.group && !$scope.sponsors[i].schoolPathRegex) || $scope.sponsors[i].schoolPathRegex.test(userSchoolPath);
 		}
 		
 		$scope.getShown();
@@ -50,6 +61,8 @@ angular.module('tradity').
 		$scope.$on('user-update', function(event, data) {
 			$scope.getShown(data);
 		});
-
-		$scope.group = !!($scope.$parent && $scope.$parent.$parent && $scope.$parent.$parent.school);
+		
+		$scope.$on('$stateChangeSuccess', function() {
+			$scope.getShown();
+		});
 	});

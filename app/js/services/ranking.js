244a8971c3ba416b876bb1dce03376765ac73fcc
Barne Jürgensen <barne.juergensen@gmail.com>
2014-09-24 15:42:56 +0200

    add more dummy services

---
+++ app/js/services/ranking.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name tradity.ranking
+ * @description
+ * # handels the ranking group/user etc
+ * Factory
+ */
+angular.module('tradity')
+	.factory('ranking', function ($q) {
+		return {
+		};
+	});
---
+++ app/js/services/user.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name tradity.user
+ * @description
+ * # user
+ * Factory
+ */
+angular.module('tradity')
+	.factory('user', function ($q) {
+		return {
+		};
+	});

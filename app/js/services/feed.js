f33c9f6adf6cca99e846edea04b2c335a49dc408
Barne Jürgensen <barne.juergensen@gmail.com>
2014-12-23 19:24:25 +0100

    add feed service

---
+++ app/js/services/feed.js
@@ -0,0 +1,56 @@
+'use strict';
+
+angular.module('tradity')
+	/**
+	 * @ngdoc service
+	 * @name tradity.feed
+	 * @description
+	 * # feed
+	 * Factory in the tradityApp.
+	 */
+	.factory('feed', function ($rootScope,socket,event) {
+
+		var	$feed = $rootScope.$new(true);
+				$feed.items = [];
+		var	feedEvents = ['trade', 'watch-add', 'comment', 'dquery-exec', 'user-provchange', 'user-namechange', 'user-reset', 'mod-notification'];
+
+		var updateFeed = function(res) {
+			if (res.type == 'mod-notification') $feed.items.push(event.modNotification(res));
+			if (res.type == 'watch-add') $feed.items.push(event.watchAdd(res));
+			if (res.type == 'trade') $feed.items.push(event.trade(res));
+			if (res.type == 'comment') $feed.items.push(event.comment(res));
+			if (res.type == 'user-provchange') $feed.items.push(event.userProvchange(res));
+			if (res.type == 'user-namechange') $feed.items.push(event.userNamechange(res));
+			if (res.type == 'user-reset') $feed.items.push(event.userReset(res));
+		}
+
+		for (var i = 0; i < feedEvents.length; ++i) 
+			socket.on(feedEvents[i],updateFeed);
+
+		socket.emit('fetch-events', {
+			since: 0,
+			count: null,
+			_expect_no_response: true
+		});
+
+		return {
+			/**
+			 * @ngdoc property
+			 * @name tradity.feed#scope
+			 * @methodOf tradity.feed
+			 * @description
+			 * feed scope acces
+			 */
+			scope:$feed
+		}
+	})
+	/**
+	 * @ngdoc service
+	 * @name tradity.$feed
+	 * @description
+	 * alias of tradity.feed.scope
+	 */
+	.factory('$feed', function (feed) {
+		return feed.scope;
+	});
+

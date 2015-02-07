5e6ddc20ac24dc658c631c891d618ade59eb4d3c
Hauke Henningsen <sqrt@entless.org>
2015-02-07 11:27:46 +0100

    Cache feed events via localStorage

--- app/js/services/feed.js
+++ app/js/services/feed.js
@@ -13,14 +13,46 @@ angular.module('tradity')
 	 * Factory in the tradityApp.
 	 */
 	.factory('feed', function ($rootScope,socket,event) {
+		var localStorage_ = typeof localStorage != 'undefined' ? localStorage : {};
 
 		var	$feed = $rootScope.$new(true);
 		
 		$feed.fetch = function() {
+			// load events from localStorage cache and calculate latest known event time
+			
+			var lsFeedData = null;
+			try {
+				lsFeedData = JSON.parse(localStorage_.feed);
+			} catch (e) { console.warn(e); }
+			
+			var latestKnownEventTime = 0;
+			
+			if (!lsFeedData || lsFeedData.forUserId != $feed.forUserId) {
+				localStorage_.feed = 'null';
+			} else {
+				/* wrapped in try so that corrupt localStorage_ entries won’t break anything */
+				try {
+					// filter out event times, sort, and take last element
+					latestKnownEventTime = lsFeedData.rawItems.map(function(event) {
+						return event.eventtime;
+					}).sort().slice(-1)[0] || 0;
+					
+					for (var i = 0; lsFeedData.rawItems && i < lsFeedData.rawItems.length; ++i)
+						$feed.receiveEvent(lsFeedData.rawItems[i]);
+				} catch(e) { console.error(e); }
+			}
+			
 			socket.emit('fetch-events', {
-				since: 0,
+				since: latestKnownEventTime,
 				count: null,
 				_expect_no_response: true
+			});
+		};
+		
+		$feed.saveToLocalStorage = function() {
+			localStorage_.feed = JSON.stringify({
+				rawItems: $feed.items.map(function(ev) { return ev._origEvent; }),
+				forUserId: $feed.forUserId
 			});
 		};
 		
@@ -29,30 +61,48 @@ angular.module('tradity')
 			$feed.forUserId = null;
 		};
 		
+		$feed.clearFull = function() {
+			$feed.clear();
+			$feed.saveToLocalStorage();
+		};
+		
+		$feed.addEvent = function(ev) {
+			$feed.items.push(ev);
+			$feed.saveToLocalStorage();
+			
+			$feed.$emit('change');
+		};
+		
 		$feed.clear();
 		
-		var	feedEvents = ['trade', 'watch-add', 'comment', 'dquery-exec', 'user-provchange', 'user-namechange', 'user-reset', 'mod-notification', 'blogpost'];
-
-		var updateFeed = function(res) {
-			if (res.type == 'mod-notification') $feed.items.push(event.modNotification(res));
-			if (res.type == 'watch-add') $feed.items.push(event.watchAdd(res));
-			if (res.type == 'trade') $feed.items.push(event.trade(res));
-			if (res.type == 'comment') $feed.items.push(event.comment(res));
-			if (res.type == 'blogpost') $feed.items.push(event.blogpost(res));
-			if (res.type == 'user-provchange') $feed.items.push(event.userProvchange(res));
-			if (res.type == 'user-namechange') $feed.items.push(event.userNamechange(res));
-			if (res.type == 'user-reset') $feed.items.push(event.userReset(res));
-			$feed.$emit('change');
+		$feed.receiveEvent = function(res) {
+			var parsedEvent = null;
+			
+			if (res.type == 'mod-notification') parsedEvent = event.modNotification(res);
+			if (res.type == 'watch-add') parsedEvent = event.watchAdd(res);
+			if (res.type == 'trade') parsedEvent = event.trade(res);
+			if (res.type == 'comment') parsedEvent = event.comment(res);
+			if (res.type == 'blogpost') parsedEvent = event.blogpost(res);
+			if (res.type == 'user-provchange') parsedEvent = event.userProvchange(res);
+			if (res.type == 'user-namechange') parsedEvent = event.userNamechange(res);
+			if (res.type == 'user-reset') parsedEvent = event.userReset(res);
+			
+			if (parsedEvent) {
+				parsedEvent._origEvent = res;
+				$feed.addEvent(parsedEvent);
+			}
 		}
 
-		for (var i = 0; i < feedEvents.length; ++i) 
-			socket.on(feedEvents[i], updateFeed);
+		socket.on('*', $feed.receiveEvent);
+		
+		/* allow the remote to clear bogus client caches */
+		socket.on('clear-feed-full', $feed.clearFull);
 		
 		$rootScope.$on('user-update', function(ev, $user) {
 			if (!$user)
-				return $feed.clear();
+				return $feed.clearFull();
 			
-			if ($user.id != $feed.forUserId) {
+			if ($user.id != null && $user.id != $feed.forUserId) {
 				$feed.forUserId = $user.id;
 				$feed.fetch();
 			}

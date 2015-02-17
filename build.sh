a3752dcc04b7a5be51bc8effbd4644cba96fe037
Hauke Henningsen <sqrt@entless.org>
2015-02-17 19:02:15 +0100

    Choose local tool installations over those in the global $PATH

--- build.sh
+++ build.sh
@@ -16,7 +16,8 @@ touch app/js/controllers/adminSchools.js
 touch app/js/controllers/adminStatistics.js
 touch app/js/controllers/adminUserDetails.js
 
-command -v bower 2>/dev/null || command -v grunt 2>/dev/null || export PATH="$PATH":"node_modules/bower/bin:node_modules/grunt-cli/bin"
+command -v bower 2>/dev/null || export PATH="node_modules/bower/bin:$PATH"
+command -v grunt 2>/dev/null || export PATH="node_modules/grunt-cli/bin:$PATH"
 
 echo "var TRADITY_BUILD_STAMP = 'TDYC$(date +%s)-$(git rev-parse HEAD)';" > app/js/buildstamp.js && \
 npm --quiet install && \

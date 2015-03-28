928eee9ff624bff2245143ba8a151248034b3f21
Hauke Henningsen <sqrt@entless.org>
2015-03-28 20:40:39 +0100

    Add `translations.js` to `copy` task

--- Gruntfile.js
+++ Gruntfile.js
@@ -335,6 +335,7 @@ module.exports = function (grunt) {
 						'img/{,*/}*.{webp}',
 						'fonts/{,*/}*',
 						'js/buildstamp.js',
+						'js/translations.js',
 						'js/jit/**/*'
 					]
 				}, {

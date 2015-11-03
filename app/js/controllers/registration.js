0ac561edb354a23b99f406ab7b23b39ae0be62ab
Nico Haesler <nico@entless.org>
2015-11-03 21:43:29 +0100

    fix double parameter crashing due to strict mode

--- app/js/controllers/registration.js
+++ app/js/controllers/registration.js
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 angular.module('tradity').
-controller('RegistrationCtrl', function($scope, $stateParams, $state, user, dialogs, safestorage, gettext, languageManager, asyncLoadJS, socket, user) {
+controller('RegistrationCtrl', function($scope, $stateParams, $state, user, dialogs, safestorage, gettext, languageManager, asyncLoadJS, socket) {
 	var vm = this;
 	vm.validateStatus = {
 		name: '',

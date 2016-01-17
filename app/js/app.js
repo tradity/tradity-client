d98d64db35786138ed30de05e4b3dfffe9f1b043
Nico Haesler <nico@entless.org>
2016-01-17 21:45:48 +0100

    fix template URLs

--- app/js/app.js
+++ app/js/app.js
@@ -50,42 +50,42 @@ angular.module('tradity', [
   $stateProvider.
     state('index', {
       url: '/',
-      templateUrl: 'templates/index.html',
+      templateUrl: 'app/templates/index.html',
       controller: 'IndexCtrl'
     }).
     state('index.login', {
       url: 'login',
-      templateUrl: 'templates/login.html',
+      templateUrl: 'app/templates/login.html',
       controller: 'LoginCtrl',
       controllerAs: 'login'
     }).
     state('index.loginverif', {
       url: 'login/:emailVerifCode/:uid',
-      templateUrl: 'templates/login.html',
+      templateUrl: 'app/templates/login.html',
       controller: 'LoginCtrl',
       controllerAs: 'login'
     }).
     state('register', {
       url: '/register',
-      templateUrl: 'templates/registration.html',
+      templateUrl: 'app/templates/registration.html',
       controller: 'RegistrationCtrl',
       controllerAs: 'registration'
     }).
     state('register.step1', {
       url: '/step1',
-      templateUrl: 'templates/registration.step1.html',
+      templateUrl: 'app/templates/registration.step1.html',
       // controller: 'RegistrationCtrl',
       // controllerAs: 'step1'
     }).
     state('register.step2', {
       url: '/step2',
-      templateUrl: 'templates/registration.step2.html',
+      templateUrl: 'app/templates/registration.step2.html',
       // controller: 'RegistrationCtrl',
       // controllerAs: 'registration'
     }).
     state('survey', {
       url: '/survey/:questionnaire',
-      templateUrl: 'templates/survey.html',
+      templateUrl: 'app/templates/survey.html',
       controller: 'SurveyCtrl',
       controllerAs: 'survey'
     }).
@@ -93,54 +93,54 @@ angular.module('tradity', [
     //change to register/:inviteCode
     state('invite', {
       url: '/join/:inviteCode',
-      templateUrl: 'templates/registration.html',
+      templateUrl: 'app/templates/registration.html',
       controller: 'RegistrationCtrl',
       controllerAs: 'registration'
     }).
     state('schoolregister', {
       url: '/register/s{schoolid:SchoolID}',
-      templateUrl: 'templates/registration.html',
+      templateUrl: 'app/templates/registration.html',
       controller: 'RegistrationCtrl',
       controllerAs: 'registration'
     }).
     state('game', {
       abstract: true,
-      templateUrl: 'templates/game.html'
+      templateUrl: 'app/templates/game.html'
     }).
     state('game.feed', {
       url: '/feed',
-      templateUrl: 'templates/feed.html',
+      templateUrl: 'app/templates/feed.html',
       controller: 'FeedCtrl',
       controllerAs: 'feed'
     }).
     state('game.learning', {
       url: '/learning',
-      templateUrl: 'templates/learning.html',
+      templateUrl: 'app/templates/learning.html',
       controller: 'LearningCtrl'
     }).
     state('game.learning.catalog', {
       url: '/catalog',
-      templateUrl: 'templates/learning.catalog.html',
+      templateUrl: 'app/templates/learning.catalog.html',
       controller: 'LearningCtrl'
     }).
     state('game.learning.questions', {
       url: '/catalog/:id',
-      templateUrl: 'templates/learning.questions.html',
+      templateUrl: 'app/templates/learning.questions.html',
       controller: 'LearningCtrl'
     }).
     state('game.search', {
       url: '/search/:query',
-      templateUrl: 'templates/search.html',
+      templateUrl: 'app/templates/search.html',
       controller: 'SearchCtrl'
     }).
     state('game.chats', {
       url: '/chats/',
-      templateUrl: 'templates/chats.html',
+      templateUrl: 'app/templates/chats.html',
       controller: 'ChatCtrl'
     }).
     state('game.chat', {
       url: '/chat',
-      templateUrl: 'templates/chat.html',
+      templateUrl: 'app/templates/chat.html',
       controller: 'ChatCtrl'
     }).
     state('game.chat.id', {
@@ -153,153 +153,153 @@ angular.module('tradity', [
     }).
     state('game.options', {
       url: '/options',
-      templateUrl: 'templates/options.html',
+      templateUrl: 'app/templates/options.html',
       controller: 'OptionsCtrl'
     }).
     state('game.depot', {
       abstract: true,
       url: '/depot',
-      templateUrl: 'templates/depot.html',
+      templateUrl: 'app/templates/depot.html',
       controller: 'DepotCtrl'
     }).
     state('game.depot.overview', {
       url: '/overview',
-      templateUrl: 'templates/depot.overview.html'
+      templateUrl: 'app/templates/depot.overview.html'
     }).
     state('game.depot.listing', {
       url: '/listing',
-      templateUrl: 'templates/depot.listing.html'
+      templateUrl: 'app/templates/depot.listing.html'
     }).
     state('game.depot.transactions', {
       url: '/transactions',
-      templateUrl: 'templates/depot.transactions.html'
+      templateUrl: 'app/templates/depot.transactions.html'
     }).
     state('game.depot.transactionlog', {
       url: '/transactionlog',
-      templateUrl: 'templates/depot.transactionlog.html',
+      templateUrl: 'app/templates/depot.transactionlog.html',
       controller: 'DepotTransactionLogCtrl'
     }).
     state('game.depot.watchlist', {
       url: '/watchlist',
-      templateUrl: 'templates/depot.watchlist.html'
+      templateUrl: 'app/templates/depot.watchlist.html'
     }).
     state('game.trade', {
       url: '/trade',
-      templateUrl: 'templates/trade.html',
+      templateUrl: 'app/templates/trade.html',
       controller: 'TradeCtrl',
             controllerAs: 'trade'
     }).
     state('game.tradesellbuy', {
       url: '/trade/:sellbuy/:stockId/:amount',
-      templateUrl: 'templates/trade.html',
+      templateUrl: 'app/templates/trade.html',
       controller: 'TradeCtrl',
             controllerAs: 'trade'
     }).
     state('game.ranking', {
       url: '/ranking/:spec',
-      templateUrl: 'templates/ranking.html',
+      templateUrl: 'app/templates/ranking.html',
       controller: 'RankingCtrl'
     }).
     state('game.profile', {
       url: '/user/:userId',
-      templateUrl: 'templates/profile.html',
+      templateUrl: 'app/templates/profile.html',
       controller: 'ProfileCtrl'
     }).
     state('game.profile.overview', {
       url: '/overview',
-      templateUrl: 'templates/profile.overview.html',
+      templateUrl: 'app/templates/profile.overview.html',
       controller: 'ProfileOverviewCtrl'
     }).
     state('game.profile.history', {
       url: '/history',
-      templateUrl: 'templates/profile.history.html'
+      templateUrl: 'app/templates/profile.history.html'
     }).
     state('game.profile.pinboard', {
       url: '/pinboard',
-      templateUrl: 'templates/profile.pinboard.html',
+      templateUrl: 'app/templates/profile.pinboard.html',
       controller: 'CommentCtrl'
     }).
     state('game.profile.achievements', {
       url: '/achievements',
-      templateUrl: 'templates/profile.achievements.html',
+      templateUrl: 'app/templates/profile.achievements.html',
       controller: 'AchievementsCtrl'
     }).
     state('game.profile.achievements.categories', {
       url: '/categories',
-      templateUrl: 'templates/profile.achievements.categories.html',
+      templateUrl: 'app/templates/profile.achievements.categories.html',
       controller: 'AchievementsCtrl'
     }).
     state('game.profile.achievements.list', {
       url: '/list/:id',
-      templateUrl: 'templates/profile.achievements.list.html',
+      templateUrl: 'app/templates/profile.achievements.list.html',
       controller: 'AchievementsCtrl'
     }).
     state('admin', {
       abstract: true,
       url: '/admin',
-      templateUrl: 'templates/admin.html',
+      templateUrl: 'app/templates/admin.html',
       controller: 'AdminCtrl'
     }).
     state('admin.userlist', {
       url: '/userlist',
-      templateUrl: 'templates/admin.userlist.html',
+      templateUrl: 'app/templates/admin.userlist.html',
       controller: 'AdminUserlistCtrl'
     }).
     state('admin.statistics', {
       url: '/statistics',
-      templateUrl: 'templates/admin.statistics.html',
+      templateUrl: 'app/templates/admin.statistics.html',
       controller: 'AdminStatisticsCtrl'
     }).
     state('admin.events', {
       url: '/events',
-      templateUrl: 'templates/admin.events.html',
+      templateUrl: 'app/templates/admin.events.html',
       controller: 'AdminEventsCtrl'
     }).
     state('admin.notifications', {
       url: '/notifications',
-      templateUrl: 'templates/admin.notifications.html',
+      templateUrl: 'app/templates/admin.notifications.html',
       controller: 'AdminNotificationsCtrl'
     }).
     state('admin.schools', {
       url: '/schools',
-      templateUrl: 'templates/admin.schools.html',
+      templateUrl: 'app/templates/admin.schools.html',
       controller: 'AdminSchoolsCtrl'
     }).
     state('admin.userdetails', {
       url: '/userdetails/:uid',
-      templateUrl: 'templates/admin.userdetails.html',
+      templateUrl: 'app/templates/admin.userdetails.html',
       controller: 'AdminUserDetailsCtrl'
     }).
     state('game.groupOverview', {
       url: '/s/',
-      templateUrl: 'templates/groupOverview.html',
+      templateUrl: 'app/templates/groupOverview.html',
       controller: 'GroupOverviewCtrl'
     }).
     state('game.group', {
       url: '/s{schoolid:SchoolID}',
-      templateUrl: 'templates/group.html',
+      templateUrl: 'app/templates/group.html',
       controller: 'GroupCtrl'
     }).
     state('game.group.pinboard', {
       url: '/+pinboard',
-      templateUrl: 'templates/group.pinboard.html',
+      templateUrl: 'app/templates/group.pinboard.html',
       controller: 'CommentCtrl'
     }).
     state('game.group.ranking', {
       url: '/+ranking/:spec',
-      templateUrl: 'templates/ranking.html',
+      templateUrl: 'app/templates/ranking.html',
       controller: 'RankingCtrl'
     }).
     state('error', {
-      templateUrl: 'templates/error.html'
+      templateUrl: 'app/templates/error.html'
     }).
     state('error.404', {
       url:'/error/404',
-      templateUrl: 'templates/error.404.html'
+      templateUrl: 'app/templates/error.404.html'
     }).
     state('error.connection', {
       url:'/error/connection',
-      templateUrl: 'templates/error.connection.html'
+      templateUrl: 'app/templates/error.connection.html'
     }).
     state('unknownEntity', {
       url:'{entity:GenericEntityID}',
@@ -318,10 +318,10 @@ angular.module('tradity', [
   
   languageManager.setCurrentLanguage(null);
   
-  $templateCache.put('/dialogs/error.html',   "<ng-include src=\"'templates/dialogs/error.html'\"></ng-include>");
-  $templateCache.put('/dialogs/wait.html',    "<ng-include src=\"'templates/dialogs/wait.html'\"></ng-include>");
-  $templateCache.put('/dialogs/notify.html',  "<ng-include src=\"'templates/dialogs/notify.html'\"></ng-include>");
-  $templateCache.put('/dialogs/confirm.html', "<ng-include src=\"'templates/dialogs/confirm.html'\"></ng-include>");
+  $templateCache.put('/dialogs/error.html',   "<ng-include src=\"'app/templates/dialogs/error.html'\"></ng-include>");
+  $templateCache.put('/dialogs/wait.html',    "<ng-include src=\"'app/templates/dialogs/wait.html'\"></ng-include>");
+  $templateCache.put('/dialogs/notify.html',  "<ng-include src=\"'app/templates/dialogs/notify.html'\"></ng-include>");
+  $templateCache.put('/dialogs/confirm.html', "<ng-include src=\"'app/templates/dialogs/confirm.html'\"></ng-include>");
   
   $rootScope.loadState = false;
   $rootScope.$on("$stateChangeStart", function () {

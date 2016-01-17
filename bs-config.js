b457ee746f3d5eb6fd75fc1e3954f8ac1879c713
Nico Haesler <nico@entless.org>
2016-01-17 23:52:10 +0100

    add spa html5 history api fallback

--- bs-config.js
+++ bs-config.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     "proxy": false,
     "port": 3000,
-    "middleware": false,
+    "middleware": require('connect-history-api-fallback')(),
     "serveStatic": [],
     "ghostMode": {
         "clicks": true,

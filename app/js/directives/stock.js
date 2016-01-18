243ba395742ef8c654a01520dc4b0d8bc0ae8916
Anna Henningsen <sqrt@entless.org>
2016-01-18 22:36:12 +0100

    Style fixups for b6d1604d87

--- app/js/directives/stock.js
+++ app/js/directives/stock.js
@@ -1,23 +1,20 @@
-'use strict';
-
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+(function() { 'use strict';
 
 angular.module('tradity').
   directive('stock', function($compile,stock) {
     function link(scope, element, attrs) {
-      console.log(element)
       scope.template = false;
       var enter = function() {
-        console.log(attrs.isin)
-        stock.getComments(attrs.isin).then(function(comments){
+        stock.getComments(attrs.isin).then(function(comments) {
           scope.comments = comments;
-          scope.template = 'templates/popover.stock.html'
-          console.log(comments)
-        })
+          scope.template = 'templates/popover.stock.html';
+        });
+        
         var el = $('.popover',this);
-        el.show()
+        el.show();
         var pos = $(this).offset();
         var h = $(this).height();
         var w = $(this).width();
@@ -26,19 +23,21 @@ angular.module('tradity').
         var arrowOff = 0;
         if(left < 0) {
           arrowOff = left-(w/2);
-          el.offset({ top: top, left: 0 })
-          $('.arrow',el).css('margin-left',arrowOff)
-        }
-        else
+          el.offset({ top: top, left: 0 });
+          $('.arrow',el).css('margin-left', arrowOff);
+        } else {
           el.offset({ top: top, left: left });
-        element.unbind('mouseenter')
-        element.bind('mouseout',function(){
-          element.unbind('mouseout')
+        }
+        
+        element.unbind('mouseenter');
+        element.bind('mouseout', function() {
+          element.unbind('mouseout');
           el.hide();
-          element.bind('mouseenter',enter)
-        })
-      }
-      element.bind('mouseenter',enter)
+          element.bind('mouseenter', enter);
+        });
+      };
+      
+      element.bind('mouseenter', enter);
     }
     
     return {
@@ -50,3 +49,5 @@ angular.module('tradity').
       }
     };
   });
+
+})();

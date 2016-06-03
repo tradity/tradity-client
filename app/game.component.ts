54f136bd0668978096b32ba172a1bdd0baf52af6
Nico Haesler <nico@entless.org>
2016-06-03 14:45:02 +0200

    first work on sidenav

---
+++ app/game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MdToolbar } from '@angular2-material/toolbar';
+import { MdButton } from '@angular2-material/button';
+
+@Component({
+  selector: 'tradity-game',
+  templateUrl: 'app/game.component.html'
+})
+export class GameComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+
+}

6c12cd260a753b027606fab175bcba60f8ac0eb2
Nico Haesler <nico@entless.org>
2016-07-15 14:02:51 +0200

    redirect to login upon auth error

--- app/api.service.ts
+++ app/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, RequestOptionsArgs, RequestOptions, Headers, Response } from '@angular/http';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
@@ -8,9 +9,14 @@ export class ApiService {
   private options: RequestOptions;
   private baseUrl = 'https://dev.tradity.de:443/api/v1';
 
-  constructor(private http: Http) {
+  constructor(private http: Http, private router: Router) {
     let headers = new Headers({ 'Content-Type': 'application/json' });
     this.options = new RequestOptions({ headers: headers });
+  }
+  
+  private handleError = (error: any) => {
+    if (error.status == 401) this.router.navigateByUrl('login');
+    return Observable.throw('Login required');
   }
   
   setAuthToken(token: string) : void {
@@ -22,10 +28,10 @@ export class ApiService {
   }
   
   get(url: string) : Observable<Response> {
-    return this.http.get(this.baseUrl + url, this.options);
+    return this.http.get(this.baseUrl + url, this.options).catch(this.handleError);
   }
   
   post(url: string, body: any) : Observable<Response> {
-    return this.http.post(this.baseUrl + url, JSON.stringify(body), this.options);
+    return this.http.post(this.baseUrl + url, JSON.stringify(body), this.options).catch(this.handleError);
   }
 }

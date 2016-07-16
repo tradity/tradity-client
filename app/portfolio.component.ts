c2707856c936f8432089f8b954bffc810ba2a98d
Nico Haesler <nico@entless.org>
2016-07-16 23:49:18 +0200

    prototype portfolio into observable data service

--- app/portfolio.component.ts
+++ app/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 import { StocksService } from './stocks.service';
 
@@ -8,9 +9,14 @@ import { StocksService } from './stocks.service';
   providers: [StocksService]
 })
 export class PortfolioComponent implements OnInit {
+  
+  portfolio: Observable<any>;
+  
   constructor(private stocksService: StocksService) { }
 
   ngOnInit() {
+    this.portfolio = this.stocksService.portfolio;
+    this.portfolio.subscribe(val => console.log("received", val));
     this.stocksService.loadPortfolio();
   }
 
--- app/stocks.service.ts
+++ app/stocks.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
 import { ApiService } from './api.service';
 
 @Injectable()
 export class StocksService {
   
-  private _portfolio;
-  private _transactions;
+  private _portfolio: Subject<any>;
+  private _transactions: Subject<any>;
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) { 
+    this._portfolio = Subject.create();
+  }
   
   get portfolio() {
     return this._portfolio.asObservable();
@@ -21,7 +24,7 @@ export class StocksService {
   loadPortfolio(): void {
     this.apiService.get('/depot')
     .map(res => res.json())
-    .subscribe(res => console.log(res));
+    .subscribe(res => this._portfolio.next(res));
   }
   
   loadTransactions(): void {

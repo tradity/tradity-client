7e729fbbc62e51cd33ae9a9682f56ea478858fc9
Nico Haesler <nico@entless.org>
2016-07-21 23:49:06 +0200

    use observables with ranking

--- app/ranking.service.ts
+++ app/ranking.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
 import { ApiService } from './api.service';
 
 @Injectable()
 export class RankingService {
+  
+  private _ranking: Subject<any>;
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) { 
+    this._ranking = Subject.create();
+  }
+  
+  get ranking() {
+    return this._ranking.asObservable();
+  }
   
   load() {
     this.apiService.get('/ranking')
     .map(res => res.json())
-    .subscribe(res => console.log(res));
+    .subscribe(res => this._ranking.next(res.data));
   }
 
 }

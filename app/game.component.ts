16c8d1f6e739a807d1bd76201d4e24b374d70d54
Nico Haesler <nico@entless.org>
2016-08-28 20:57:01 +0200

    remove material design

--- app/game.component.ts
+++ app/game.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ROUTER_DIRECTIVES } from '@angular/router';
-import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav/sidenav';
-import { MdToolbar } from '@angular2-material/toolbar';
-import { MdButton } from '@angular2-material/button';
 
 import { UserService } from './user.service';
 import { SearchComponent } from './search.component';
@@ -10,7 +7,7 @@ import { SearchComponent } from './search.component';
 @Component({
   selector: 'tradity-game',
   templateUrl: 'app/game.component.html',
-  directives: [ROUTER_DIRECTIVES, MD_SIDENAV_DIRECTIVES, MdButton]
+  directives: [ROUTER_DIRECTIVES]
 })
 export class GameComponent implements OnInit {
   constructor(private userService: UserService, private router: Router) { }

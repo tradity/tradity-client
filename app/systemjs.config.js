532baf001c61016bd6d75bb127bec690e0113471
Nico Haesler <nico@entless.org>
2016-09-20 01:16:09 +0200

    update systemjs config

--- app/systemjs.config.js
+++ app/systemjs.config.js
@@ -1,55 +1,28 @@
 (function (global) {
-  var map = {
-    'app':                'app',    
-    '@angular':           'node_modules/@angular',
-    'rxjs':               'node_modules/rxjs',    
-    '@angular2-material': 'node_modules/@angular2-material'
-  };
-  
-  var packages = {
-    'app':                        { main: 'main.js', defaultExtension: 'js' },
-    'rxjs':                       { defaultExtension: 'js' }
-  };
-  
-  var ngPackageNames = [
-    'common',
-    'compiler',
-    'core',
-    'forms',
-    'http',
-    'platform-browser',
-    'platform-browser-dynamic',
-    'router'
-  ];
-  
-  // Individual files (~300 requests):
-  function packIndex(pkgName) {
-    packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
-  }
-  // Bundled (~40 requests):
-  function packUmd(pkgName) {
-    packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-  }
-  // Most environments should use UMD; some (Karma) need the individual index files
-  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-  // Add package entries for angular packages
-  ngPackageNames.forEach(setPackageConfig);
-  
-  var ngMaterialPackageNames = [
-    'core',
-    'input',
-    'button',
-    'sidenav'
-  ];
-  
-  ngMaterialPackageNames.forEach(function (pkgName) {
-    packages['@angular2-material/' + pkgName] = { main: pkgName + '.js', format: 'cjs', defaultExtension: 'js' };
+  System.config({
+    paths: {
+      'npm:': 'node_modules/'
+    },
+    map: {
+      app: 'app',
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      'rxjs': 'npm:rxjs'
+    },
+    packages: {
+      app: {
+        main: './main.js',
+        defaultExtension: 'js'
+      },
+      rxjs: {
+        defaultExtension: 'js'
+      }
+    }
   });
-  
-  var config = {
-    map: map,
-    packages: packages
-  }
-  
-  System.config(config);
 })(this);

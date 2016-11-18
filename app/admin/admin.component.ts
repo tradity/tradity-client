f8936656f71790ed4b819b24712922226ee63743
Nico Haesler <nico@entless.org>
2016-11-18 16:58:02 +0100

    add admin module

---
+++ app/admin/admin.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tradity-admin',
+  templateUrl: 'admin.component.html'
+})
+export class AdminComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
---
+++ app/admin/admin.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { AdminComponent } from './admin.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [AdminComponent],
+  providers: [],
+})
+export class AdminModule { }

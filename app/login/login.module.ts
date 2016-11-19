b04669d02e8d8a8ade0cea21ea232c92c908ec92
Nico Haesler <nico@entless.org>
2016-11-19 12:46:27 +0100

    fix whitespace

--- app/login/login.module.ts
+++ app/login/login.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { LoginComponent }   from './login.component';
+import { LoginComponent } from './login.component';
 import { routing } from './login.routing';
 
 @NgModule({

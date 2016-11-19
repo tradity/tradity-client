a52d16014447c1257e76f6851694a0fdb5dd1fc0
Nico Haesler <nico@entless.org>
2016-11-19 22:37:19 +0100

    move portfolio module to game module

--- app/game/game.module.ts
+++ app/game/game.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { routing } from './game.routing';
 import { GameComponent } from './game.component';
+import { PortfolioModule } from '../portfolio/portfolio.module';
 
 @NgModule({
-  imports: [routing],
+  imports: [
+    routing,
+    PortfolioModule
+  ],
   exports: [],
   declarations: [GameComponent],
   providers: [],

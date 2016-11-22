cb5d1c041a59f99d962671d7f3df1ba49119cd72
Nico Haesler <nico@entless.org>
2016-11-22 14:09:59 +0100

    add registration styles

--- app/registration/registration.component.ts
+++ app/registration/registration.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tradity-registration',
-  templateUrl: 'app/registration/registration.component.html'
+  templateUrl: 'app/registration/registration.component.html',
+  styleUrls: ['app/registration/registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
   constructor() { }

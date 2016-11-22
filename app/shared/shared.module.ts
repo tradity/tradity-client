4132feeb929af630ba593929ed66c82ab8debf11
Nico Haesler <nico@entless.org>
2016-11-22 16:13:01 +0100

    refactor bottom nav into component

--- app/shared/shared.module.ts
+++ app/shared/shared.module.ts
@@ -3,18 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormComponent } from './form.component';
 import { SubnavComponent } from './subnav.component';
 import { TableComponent } from './table.component';
+import { BottomNavComponent } from './bottom-nav.component';
 
 @NgModule({
   imports: [],
   exports: [
     FormComponent,
     SubnavComponent,
-    TableComponent
+    TableComponent,
+    BottomNavComponent
   ],
   declarations: [
     FormComponent,
     SubnavComponent,
-    TableComponent
+    TableComponent,
+    BottomNavComponent
   ],
   providers: [],
 })

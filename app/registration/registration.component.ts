a458b25bb637911b497ac2b9d988342f90a39ede
Nico Haesler <nico@entless.org>
2016-11-22 14:05:15 +0100

    add registration

---
+++ app/registration/registration.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tradity-registration',
+  templateUrl: 'app/registration/registration.component.html'
+})
+export class RegistrationComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}

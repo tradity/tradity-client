0daaba7249727a3c89a4297b5d533f37e899fd74
Nico Haesler <nico@entless.org>
2016-12-27 11:57:36 +0100

    create stocks module

--- app/game/game.module.ts
+++ app/game/game.module.ts
@@ -2,18 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { routing } from './game.routing';
 import { GameComponent } from './game.component';
-import { StocksComponent } from './stocks.component';
-import { SharedModule } from '../shared/shared.module';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
-    routing,
-    SharedModule,
-    CommonModule
+    routing
   ],
   exports: [],
-  declarations: [GameComponent, StocksComponent],
+  declarations: [GameComponent],
   providers: [],
 })
 export class GameModule { }

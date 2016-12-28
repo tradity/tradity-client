92cacc8c60e5ab71a0ddb22bbcfcd49a56697968
Nico Haesler <nico@entless.org>
2016-12-28 21:45:00 +0100

    add stock detail component

---
+++ app/stocks/stock-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tradity-stock-detail',
+  templateUrl: 'app/stocks/stock-detail.component.html'
+})
+export class StockDetailComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}

b00c72abaa448bbc815c312f2b017cbfb375b215
Nico Haesler <nico@entless.org>
2016-12-29 15:06:14 +0100

    populate ranking with actual data

--- app/ranking/ranking.module.ts
+++ app/ranking/ranking.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { routing } from './ranking.routing';
 import { RankingComponent }   from './ranking.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     routing
   ],
   exports: [],

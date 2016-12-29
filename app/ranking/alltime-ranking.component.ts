8466e3b5727044f523979105a320144aa78448d3
Nico Haesler <nico@entless.org>
2016-12-29 17:48:58 +0100

    add all-time ranking

---
+++ app/ranking/alltime-ranking.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tradity-alltime-ranking',
+  templateUrl: 'app/ranking/alltime-ranking.component.html'
+})
+export class AlltimeRankingComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}

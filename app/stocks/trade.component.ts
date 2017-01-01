d8bcfb2b4f8053f593656a54bde8a2b5c2517fdd
Nico Haesler <nico@entless.org>
2017-01-01 23:47:56 +0100

    add stock data to trade view

--- app/stocks/trade.component.ts
+++ app/stocks/trade.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
+import { StocksService } from '../stocks.service';
 
 @Component({
   selector: 'tradity-trade',
   templateUrl: 'app/stocks/trade.component.html'
 })
-export class TradeComponent implements OnInit {
-  constructor() { }
+export class TradeComponent implements OnInit, OnDestroy {
+  private stockSubscription: Subscription;
+  private stock = {};
 
-  ngOnInit() { }
+  constructor(private stocksService: StocksService) { }
+
+  ngOnInit() {
+    this.stockSubscription = this.stocksService.stock('US88160R1014')
+                             .subscribe(res => this.stock = res);
+    this.stocksService.loadStock('US88160R1014');
+  }
+
+  ngOnDestroy() {
+    this.stockSubscription.unsubscribe();
+  }
 }

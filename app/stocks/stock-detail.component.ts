ed3783f45ca84e277053af9a38579ec4e1be7681
Nico Haesler <nico@entless.org>
2017-01-01 23:57:07 +0100

    show actual data on stock detail view

--- app/stocks/stock-detail.component.ts
+++ app/stocks/stock-detail.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
+import { StocksService } from '../stocks.service';
 
 @Component({
   selector: 'tradity-stock-detail',
   templateUrl: 'app/stocks/stock-detail.component.html'
 })
-export class StockDetailComponent implements OnInit {
-  constructor() { }
+export class StockDetailComponent implements OnInit, OnDestroy {
+  private stockSubscription: Subscription;
+  private stock = {};
 
-  ngOnInit() { }
+  constructor(private stocksService: StocksService) { }
+
+  ngOnInit() {
+    this.stockSubscription = this.stocksService.stock('US88160R1014')
+                             .subscribe(res => this.stock = res);
+    this.stocksService.loadStock('US88160R1014');
+  }
+
+  ngOnDestroy() {
+    this.stockSubscription.unsubscribe();
+  }
 }

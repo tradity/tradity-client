3a757da20853f96fa3aa13f1b00fdb43b17b0611
Nico Haesler <nico@entless.org>
2017-01-04 00:01:30 +0100

    remove unused code

--- app/shared/container-control.component.ts
+++ app/shared/container-control.component.ts
@@ -8,11 +8,9 @@ import { ContainerComponent } from './container.component';
   styleUrls: ['app/shared/container-control.component.css']
 })
 export class ContainerControlComponent {
-  @Output() onToggle = new EventEmitter();
   @HostBinding('class.active') isActive: boolean = false;
   @HostListener('click') onClick() {
     this.isActive = this.isActive ? false : true;
-    this.onToggle.emit();
     this.container.onToggle();
   }
 

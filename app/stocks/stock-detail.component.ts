d980ef8326ae3abeb21a72907b93399d8d5e29ee
Nico Haesler <nico@entless.org>
2017-01-03 10:37:41 +0100

    read isin from route on stock detail view

--- app/stocks/stock-detail.component.ts
+++ app/stocks/stock-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -12,11 +13,12 @@ export class StockDetailComponent implements OnInit, OnDestroy {
   private stockSubscription: Subscription;
   private stock = {};
 
-  constructor(private stocksService: StocksService) { }
+  constructor(private route: ActivatedRoute, private stocksService: StocksService) { }
 
   ngOnInit() {
-    this.stockSubscription = this.stocksService.stock('US88160R1014')
-                             .subscribe(res => this.stock = res);
+    this.stockSubscription = this.route.params
+      .switchMap((params: Params) => this.stocksService.stock(params['isin']))
+      .subscribe(res => this.stock = res);
   }
 
   ngOnDestroy() {

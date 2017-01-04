c11563720cd15f5546ecb255e8b071c0cbbc5778
Nico Haesler <nico@entless.org>
2017-01-04 22:04:00 +0100

    hide overflows on various list elements

--- app/shared/list-item-value-list.component.ts
+++ app/shared/list-item-value-list.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tradity-li-vl',
   template: '<ng-content></ng-content>',
-  styles: [':host {display: flex; flex-wrap: wrap; flex-basis: 100%; }']
+  styles: [':host {display: flex; flex-wrap: wrap; flex-basis: 100%; overflow: hidden; }']
 })
 export class ListItemValueListComponent {
   constructor() { }

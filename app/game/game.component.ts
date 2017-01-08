b12187f6be423c83ca1846b2ad88fc15e2ccbba7
Nico Haesler <nico@entless.org>
2017-01-08 23:48:08 +0100

    display user data in navigation

--- app/game/game.component.ts
+++ app/game/game.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 import { UserService } from '../user.service';
 import { SearchComponent } from '../search.component';
@@ -9,17 +10,24 @@ import { SearchComponent } from '../search.component';
   templateUrl: 'app/game/game.component.html',
   styleUrls: ['app/game/game.component.css']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, OnDestroy {
   private isMenuOpen = false;
+  private ownUserSubscription: Subscription;
+  private ownUser = {};
   
   constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+    this.ownUserSubscription = this.userService.ownUser.subscribe(res => this.ownUser = res);
     this.router.events.subscribe(val => {
     if (NavigationStart) this.isMenuOpen = false;
     else this.isMenuOpen = true;
     })
-   }
+  }
+
+  ngOnDestroy() {
+    this.ownUserSubscription.unsubscribe();
+  }
   
   logout() {
     this.userService.logout();

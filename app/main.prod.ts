15167f0fc6bbdd2c7c030282deaa5039a5523336
Nico Haesler <nico@entless.org>
2017-01-14 22:28:46 +0100

    enable angular's prod mode in production

--- app/main.prod.ts
+++ app/main.prod.ts
@@ -1,4 +1,6 @@
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
+import { enableProdMode } from '@angular/core';
 
+enableProdMode();
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);

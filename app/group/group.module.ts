119247ecd8ea191ca98bbdad0a5fa2bf1dfbd3da
Nico Haesler <nico@entless.org>
2017-02-02 20:52:24 +0100

    add group routing

--- app/group/group.module.ts
+++ app/group/group.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
+import { SharedModule } from '../shared/shared.module';
 import { GroupComponent } from './group.component';
 
 @NgModule({
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule
+  ],
   exports: [],
   declarations: [GroupComponent],
   providers: [],

1d26aec73aa550b6de553dcd2536bd8b9306f71d
Nico Haesler <nico@entless.org>
2017-02-19 18:33:52 +0100

    implement managing groups in admin interface

--- app/admin.service.ts
+++ app/admin.service.ts
@@ -21,4 +21,17 @@ export class AdminService {
     .map(res => res.json().data)
     .subscribe(res => this._userlist.next(res));
   }
+
+  deleteGroup(gid: number) {
+    return this.apiService.post('/school/null/merge/' + gid, {})
+    .map(res => res.json());
+  }
+
+  createSchool(name: string, path: string) {
+    return this.apiService.post('/school', {
+      schoolname: name,
+      schoolpath: path
+    })
+    .map(res => res.json());
+  }
 }
---
+++ app/admin/admin-groups.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { AdminService } from '../admin.service';
+import { GroupService } from '../group.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'tradity-admin-groups',
+  templateUrl: 'admin-groups.component.html',
+  styles: ['table, th, td { border: 1px solid black; }']
+})
+export class AdminGroupsComponent implements OnInit {
+  name: string;
+  path: string;
+  groups: Observable<any>;
+
+  constructor(private adminService: AdminService, private groupService: GroupService) { }
+
+  ngOnInit() {
+    this.groups = this.groupService.groupList;
+  }
+
+  deleteGroup(school: any) {
+    if (!confirm('Wirklich Schule ' + school.schoolid + ' („' + school.name + '“) löschen?')) return;
+    this.adminService.deleteGroup(school.schoolid)
+    .subscribe(
+      res => {
+        alert('Gruppe erfolgreich gelöscht!');
+        this.groupService.loadGroups();
+      },
+      err => alert('Fehler beim Löschen der Gruppe')
+    );
+  }
+
+  createGroup() {
+    this.adminService.createSchool(this.name, this.path)
+    .subscribe(
+      res => {
+        alert('Gruppe erfolgreich erstellt!');
+        this.groupService.loadGroups();
+      },
+      err => alert('Fehler beim Erstellen der Gruppe')
+    );
+  }
+}

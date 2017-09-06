7eece79ffd50a26b5891a98dc469ad3e01ea92b4
Nico Haesler <nico@entless.org>
2017-09-06 18:37:46 +0200

    subscribe api service to auth key

--- app/core/api.service.ts
+++ app/core/api.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, RequestOptionsArgs, RequestOptions, Headers, Response } from '@angular/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import { getAuthKey } from '../auth/auth.reducer';
 
 @Injectable()
 export class ApiService {
@@ -9,34 +11,27 @@ export class ApiService {
   private options: RequestOptions;
   private baseUrl: string;
 
-  constructor(private http: Http, private router: Router) {
+  constructor(private http: Http, private router: Router, private store: Store<any>) {
     let headers = new Headers({ 'Content-Type': 'application/json' });
     this.options = new RequestOptions({ headers: headers });
 
-    let authToken = localStorage.getItem('authorizationToken');
-    if (authToken) this.setAuthToken(authToken);
-
     let hostname = document.location.hostname.includes('.tradity.de') ? document.location.hostname : 'dev.tradity.de';
     this.baseUrl = 'https://' + hostname + ':443/api/v1';
+
+    this.store.select(getAuthKey).subscribe((key: string) => {
+      if (key != null) this.options.headers.set('Authorization', key);
+      else this.options.headers.delete('Authorization');
+    });
   }
   
   private handleError = (error: any) => {
     if (error.status == 401) {
-      this.delAuthToken();
+      // TODO: use effect for this
+      this.options.headers.delete('Authorization');
       this.router.navigateByUrl('login');
       return Observable.throw('Login required');
     }
     return Observable.throw(error.json());
-  }
-  
-  setAuthToken(token: string) : void {
-    this.options.headers.set('Authorization', token);
-    localStorage.setItem('authorizationToken', token);
-  }
-  
-  delAuthToken() : void {
-    localStorage.removeItem('authorizationToken');
-    this.options.headers.delete('Authorization');
   }
   
   get(url: string) : Observable<Response> {

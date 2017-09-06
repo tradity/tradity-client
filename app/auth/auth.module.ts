dd8099b39a15cd5db2925f1ebbeb8d4f5a095688
Nico Haesler <nico@entless.org>
2017-09-06 17:14:17 +0200

    set up auth effects

--- app/auth/auth.module.ts
+++ app/auth/auth.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { SharedModule } from '../shared/shared.module';
 
 import { routing } from './auth.routing';
 import { authReducer } from './auth.reducer';
+import { AuthEffects } from './auth.effects';
 import { LoginComponent } from './login.component';
 
 @NgModule({
   imports: [
     FormsModule,
     StoreModule.forFeature('auth', authReducer),
+    EffectsModule.forFeature([AuthEffects]),
     SharedModule,
     routing
   ],

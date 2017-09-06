f418d78921df5b395c0b06f10eb53c4bc2c9c535
Nico Haesler <nico@entless.org>
2017-09-07 00:51:45 +0200

    load user on dashboard load

--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 
 import { getUser } from '../auth/auth.reducer';
 import { User } from '../auth/user.model';
+import * as authActions from '../auth/auth.actions';
 import { GameComponent } from '../game/game.component';
 
 @Component({
@@ -17,6 +18,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
   user: User;
 
   constructor(private store: Store<any>, private gameComponent: GameComponent) {
+    this.store.dispatch(new authActions.LoadUser());
     this.userSub = this.store.select(getUser).subscribe(user => this.user = user);
   }
 

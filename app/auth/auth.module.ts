efd820c81435b10390ebd147d614aba1c01d9881
Nico Haesler <nico@entless.org>
2017-09-06 11:26:24 +0200

    set up auth reducer

--- app/auth/auth.module.ts
+++ app/auth/auth.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
 
 import { SharedModule } from '../shared/shared.module';
 
 import { routing } from './auth.routing';
+import { authReducer } from './auth.reducer';
 import { LoginComponent } from './login.component';
 
 @NgModule({
   imports: [
     FormsModule,
+    StoreModule.forFeature('auth', authReducer),
     SharedModule,
     routing
   ],

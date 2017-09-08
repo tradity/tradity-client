8d18182c1e5c587d880279d2cfe2d94c19347763
Nico Haesler <nico@entless.org>
2017-09-08 22:36:28 +0200

    move registration to auth module

--- app/auth/auth.module.ts
+++ app/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
@@ -9,9 +10,13 @@ import { routing } from './auth.routing';
 import { authReducer } from './auth.reducer';
 import { AuthEffects } from './auth.effects';
 import { LoginComponent } from './login.component';
+import { RegistrationComponent }   from './registration.component';
+import { RegistrationStep1Component } from './registration-step1.component';
+import { RegistrationStep2Component } from './registration-step2.component';
 
 @NgModule({
   imports: [
+    CommonModule,
     FormsModule,
     StoreModule.forFeature('auth', authReducer),
     EffectsModule.forFeature([AuthEffects]),
@@ -19,7 +24,12 @@ import { LoginComponent } from './login.component';
     routing
   ],
   exports: [],
-  declarations: [LoginComponent],
+  declarations: [
+    LoginComponent,
+    RegistrationComponent,
+    RegistrationStep1Component,
+    RegistrationStep2Component
+  ],
   providers: [],
 })
 export class AuthModule { }
--- app/auth/auth.routing.ts
+++ app/auth/auth.routing.ts
@@ -2,6 +2,9 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login.component';
+import { RegistrationComponent } from './registration.component';
+import { RegistrationStep1Component } from './registration-step1.component';
+import { RegistrationStep2Component } from './registration-step2.component';
 
 const authRoutes: Routes = [
   {
@@ -11,6 +14,15 @@ const authRoutes: Routes = [
   {
     path: 'login/:emailVerifCode/:uid',
     component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent,
+    children: [
+      { path: '', redirectTo: 'step1', pathMatch: 'full' },
+      { path: 'step1', component: RegistrationStep1Component },
+      { path: 'step2', component: RegistrationStep2Component }
+    ]
   }
 ]
 

40c1fd450bad95e942d0a4730b48d36738b0b74f
Nico Haesler <nico@entless.org>
2017-09-29 23:03:35 +0200

    implement custom input element component

--- app/shared/input.component.ts
+++ app/shared/input.component.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, Input, forwardRef, HostBinding } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   moduleId: module.id,
-  selector: '[tradity-input]',
+  selector: 'tradity-input[type=text], tradity-input[type=email], tradity-input[type=password]',
   styleUrls: ['input.component.css'],
-  template: ''
+  template: '<span class="prefix"><tradity-icon *ngIf="prefix">{{ prefix }}</tradity-icon></span><input [type]="type" [value]="value" [placeholder]="placeholder" [autofocus]="autofocus" (input)="onChange($event.target.value)" (focus)="onFocus()" (blur)="onBlur()" /><span class="suffix"><tradity-icon *ngIf="prefix">{{ suffix }}</tradity-icon></span>',
+  providers: [
+    { 
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
 })
-export class InputComponent { }
+export class InputComponent implements ControlValueAccessor {
+  @Input() value: any;
+  @Input() prefix: string;
+  @Input() suffix: string;
+  @Input() type: string;
+  @Input() placeholder: string;
+  @HostBinding('class.focus') focus = false;
+
+  private _autofocus: boolean;
+  private onChange: (value: any) => void;
+  private onTouched: () => void;
+
+  @Input()
+  get autofocus(): boolean { return this._autofocus }
+  set autofocus(value) { this._autofocus = value != null }  
+
+  onFocus() {
+    this.focus = true;
+  }
+
+  onBlur() {
+    this.focus = false;
+    this.onTouched();
+  }  
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: (value: any) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+}

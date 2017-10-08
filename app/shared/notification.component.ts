f2ae9f7dddfaf941a7877e1551e89348b79ef370
Nico Haesler <nico@entless.org>
2017-10-08 23:13:04 +0200

    finish notification design

--- app/shared/notification.component.ts
+++ app/shared/notification.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostBinding } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import * as appActions from '../app.actions';
@@ -10,9 +10,11 @@ import * as appActions from '../app.actions';
   template: '<tradity-icon>{{ icon }}</tradity-icon><span>{{ message }}</span><tradity-icon (click)="close()">close</tradity-icon>'
 })
 export class NotificationComponent {
+  @HostBinding('attr.type') _type: string;
   @Input() id: number;
   @Input() message: string;
   @Input() set type(type: string) {
+    this._type = type;
     switch (type) {
       case 'info':
       case 'warning':

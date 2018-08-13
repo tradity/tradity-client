3ed56ea8f0af0ae27f601923d8d0224adf3a85ba
Nico Haesler <nico@entless.org>
2018-08-13 14:20:34 +0200

    remove unused import so that rollup is happy

--- rollup-config.js
+++ rollup-config.js
@@ -1,7 +1,6 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs    from 'rollup-plugin-commonjs';
 import uglify      from 'rollup-plugin-uglify';
-import alias       from 'rollup-plugin-alias';
 
 export default {
   input: 'tmp/app/main.prod.js',

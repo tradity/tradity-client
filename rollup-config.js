8e35798dd439e77f198e65ae7f90968063f962bc
Nico Haesler <nico@entless.org>
2018-08-13 23:14:31 +0200

    upgrade rollup and plugins

--- rollup-config.js
+++ rollup-config.js
@@ -1,6 +1,6 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs    from 'rollup-plugin-commonjs';
-import uglify      from 'rollup-plugin-uglify';
+import { uglify }  from 'rollup-plugin-uglify';
 
 export default {
   input: 'tmp/app/main.prod.js',

2628f83b33350b098da2c193375f875d2b7c75eb
Nico Haesler <nico@entless.org>
2018-08-13 14:19:24 +0200

    upgrade to angular 5

--- app/main.prod.ts
+++ app/main.prod.ts
@@ -1,5 +1,5 @@
 import { platformBrowser } from '@angular/platform-browser';
-import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
+import { AppModuleNgFactory } from './app.module.ngfactory';
 import { enableProdMode } from '@angular/core';
 
 enableProdMode();

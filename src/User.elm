2c2ce5df724534776a596d014580fd921fc774cf
Nico Haesler <nico@entless.org>
2018-10-15 16:15:25 +0200

    add user loading

--- src/User.elm
+++ src/User.elm
@@ -1,8 +1,34 @@
-module User exposing (login)
+module User exposing (User, decoder, getOwnUser, login)
 
 import Http
 import Json.Decode as Decode
+import Json.Decode.Pipeline exposing (required)
 import Json.Encode as Encode
+
+
+type alias User =
+    { id : Int
+    , schoolId : Int
+    , name : String
+    , email : String
+    , emailVerified : Bool
+    , freeMoney : Int
+    , profilePic : String
+    , totalValue : Int
+    }
+
+
+decoder : Decode.Decoder User
+decoder =
+    Decode.succeed User
+        |> required "uid" Decode.int
+        |> required "dschoolid" Decode.int
+        |> required "name" Decode.string
+        |> required "email" Decode.string
+        |> required "email_verif" Decode.bool
+        |> required "freemoney" Decode.int
+        |> required "profilepic" Decode.string
+        |> required "totalvalue" Decode.int
 
 
 login : String -> String -> Bool -> Http.Request String
@@ -18,3 +44,9 @@ login username password stayLoggedIn =
     in
     Decode.field "key" Decode.string
         |> Http.post "https://dev.tradity.de/api/v1/login" body
+
+
+getOwnUser : String -> Http.Request User
+getOwnUser session =
+    Decode.field "data" decoder
+        |> Http.get "https://dev.tradity.de/api/v1/user"

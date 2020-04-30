47de6f84e43bfa939e80a3e66f3c11f9db126709
Nico Haesler <nico@entless.org>
2020-04-30 21:54:40 +0200

    inline value list css

--- app/shared/value-list.component.ts
+++ app/shared/value-list.component.ts
@@ -4,6 +4,15 @@ import { Component, OnInit } from '@angular/core';
   moduleId: module.id,
   selector: 'tradity-vl',
   template: '<ng-content></ng-content>',
-  styleUrls: ['value-list.component.css']
+  styles: [`
+    :host {
+      display: flex;
+      flex-basis: 100%;
+      flex-wrap: wrap;
+      background-color: #2B2D38;
+      padding: 10px 0;
+      margin: 0;
+    }
+  `]
 })
 export class ValueListComponent { }

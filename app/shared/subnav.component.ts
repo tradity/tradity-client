94d96bd27c167362d6af3af037b83b4635bb790a
Nico Haesler <nico@entless.org>
2020-04-30 22:41:39 +0200

    inline subnav css

--- app/shared/subnav.component.ts
+++ app/shared/subnav.component.ts
@@ -3,7 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: '[tradity-subnav]',
-  styleUrls: ['subnav.component.css'],
+  styles: [`
+    :host >>> ul {
+      list-style: none;
+      padding: 0;
+      margin: 0 0 20px 0;
+    }
+    
+    :host >>> li {
+      display: inline;
+      margin-right: 10px;
+    }
+    
+    :host >>> a {
+      color: #170804;
+      font-size: 12px;
+      font-weight: bold;
+      line-height: 15px;
+      letter-spacing: 2px;
+      text-decoration: none;
+      text-transform: uppercase;
+      padding: 3px 7px;
+      border-radius: 5px;
+    }
+    
+    :host >>> a.active {
+      background-color: darkblue;
+      color: white;
+    }  
+  `],
   template: '<ng-content></ng-content>'
 })
 export class SubnavComponent { }

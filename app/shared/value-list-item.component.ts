c6e2e1f04ca47bc901a2fe85de6227041fd20c8f
Nico Haesler <nico@entless.org>
2020-04-30 22:33:49 +0200

    inline value list item css

--- app/shared/value-list-item.component.ts
+++ app/shared/value-list-item.component.ts
@@ -4,6 +4,33 @@ import { Component, OnInit } from '@angular/core';
   moduleId: module.id,
   selector: 'tradity-vl-li',
   template: '<ng-content></ng-content>',
-  styleUrls: ['value-list-item.component.css']
+  styles: [`
+    :host {
+      display: flex;
+      flex-basis: 50%;
+      flex-direction: column;
+      align-items: center;
+      padding: 5px 0;
+      margin: 5px 0;
+    }
+    
+    :host:nth-of-type(1),
+    :host:nth-of-type(3) {
+      flex-basis: calc(50% - 1px);
+      border-right: 1px solid rgba(151, 151, 151, 0.37);
+    }
+    
+    :host >>> span:nth-of-type(1) {
+      font-size: 16px;
+      line-height: 19px;
+      color: #F1592A;
+    }
+    
+    :host >>> span:nth-of-type(2) {
+      font-size: 10px;
+      line-height: 12px;
+      color: white;
+    }
+  `]
 })
 export class ValueListItemComponent { }

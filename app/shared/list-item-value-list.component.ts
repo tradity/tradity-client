925e426d9132a5f4f3e712cbc780f08554267111
Nico Haesler <nico@entless.org>
2020-07-08 15:59:26 +0200

    inline component css

--- app/shared/list-item-value-list.component.ts
+++ app/shared/list-item-value-list.component.ts
@@ -3,7 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tradity-li-vl',
   template: '<ng-content></ng-content>',
-  styleUrls: ['list-item-value-list.component.css']
+  styles: [`
+    :host {
+      display: flex;
+      flex-wrap: wrap;
+      flex-basis: 100%;
+      overflow: hidden;
+      font-size: 14px;
+      font-weight: bold;
+      line-height: 17px;
+    }
+    
+    :host-context(tradity-li) {
+      display: none;
+    }
+    
+    :host-context(tradity-li.expanded) {
+      display: flex;
+    }`]
 })
 export class ListItemValueListComponent {
   constructor() { }

9bc25945090e11bb05b660613e2959978936e99d
Nico Haesler <nico@entless.org>
2020-07-08 15:47:09 +0200

    inline component css

--- app/shared/button.component.ts
+++ app/shared/button.component.ts
@@ -2,8 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'button[tradity-button]',
-  styleUrls: ['button.component.css'],
+  styles: [`
+    :host[type=submit] {
+      flex-basis: 100%;
+      margin-top: 15px;
+      height: 3rem;
+      background-color: rgba(241,89,42,0.95);
+      box-shadow: 0 2px 1px 0 #F1592A;
+      border: 1px solid rgba(241,89,42,0.97);
+      border-radius: 3px;
+      color: white;
+      font-family: inherit;
+      font-size: 16px;
+      line-height: 19px;
+    }
+    
+    :host[type=submit]:disabled {
+      opacity: 0.8;
+    }`],
   template: '<ng-content></ng-content>'
 })
-
 export class ButtonComponent { }

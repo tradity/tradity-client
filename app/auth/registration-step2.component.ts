8d1add77e5327d258af06bb46913c276795a74a9
Nico Haesler <nico@entless.org>
2020-07-08 16:52:54 +0200

    inline component template

--- app/auth/registration-step2.component.ts
+++ app/auth/registration-step2.component.ts
@@ -4,7 +4,26 @@ import { RegistrationComponent } from './registration.component';
 
 @Component({
   selector: 'tradity-registration-step2',
-  templateUrl: 'registration-step2.component.html'
+  template: `
+    <form (ngSubmit)="parent.register2()" tradity-form>
+      <tradity-input type="text" placeholder="Given name" i18n-placeholder name="givenName" [(ngModel)]="parent.givenName" autofocus></tradity-input>
+      <tradity-input type="text" placeholder="Surname" i18n-placeholder name="surname" [(ngModel)]="parent.surname"></tradity-input>
+      <select name="city" [(ngModel)]="parent.city" (ngModelChange)="parent.loadSubGroups()">
+        <option selected value="">Wähle deine Metropole</option>
+        <option value="" i18n>Nur Bundeswettbewerb</option>
+        <option *ngFor="let city of parent.groupList" [value]="city.path">
+          {{city.name}}
+        </option>
+      </select>
+      <select name="school" [(ngModel)]="parent.school" *ngIf="parent.city">
+        <option selected value="" i18n>Select your school</option>
+        <option *ngFor="let school of parent.subGroups" [value]="school.path">
+          {{school.name}}
+        </option>
+      </select>
+      <tradity-input type="text" placeholder="Stadt" name="class" [(ngModel)]="parent.class"></tradity-input>
+      <button tradity-button type="submit" [disabled]="!(parent.givenName && parent.surname)" i18n>Sign up</button>
+    </form>`
 })
 export class RegistrationStep2Component {
   constructor(public parent: RegistrationComponent) { }

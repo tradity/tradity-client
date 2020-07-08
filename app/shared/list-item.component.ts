e2d7c6f55b24f3bcea273d127e482f83dc87dcdb
Nico Haesler <nico@entless.org>
2020-07-08 15:51:22 +0200

    inline component css

--- app/shared/list-item.component.ts
+++ app/shared/list-item.component.ts
@@ -3,7 +3,20 @@ import { Component, HostBinding } from '@angular/core';
 @Component({
   selector: 'tradity-li',
   template: '<ng-content></ng-content>',
-  styleUrls: ['list-item.component.css']
+  styles: [`
+    :host {
+      display: flex;
+      flex-wrap: wrap;
+      margin: 0 0 10px 60px;
+      position: relative;
+      background-color: rgba(23,8,4,0.02);
+      border-radius: 5px;
+      color: rgba(23,8,4,0.8);
+    }
+    
+    :host:last-of-type {
+      margin-bottom: 0;
+    }`]
 })
 export class ListItemComponent {
   @HostBinding('class.expanded') isExpanded: boolean = false;

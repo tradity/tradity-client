dbcf95c740ccc713885051de155bad2094199dac
Nico Haesler <nico@entless.org>
2020-07-08 15:59:00 +0200

    inline component css

--- app/shared/icon.component.ts
+++ app/shared/icon.component.ts
@@ -2,7 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'tradity-icon',
-  styleUrls: ['icon.component.css'],
+  styles: [`
+    :host {
+      font-family: 'Material Icons';
+      font-weight: normal;
+      font-style: normal;
+      font-size: 24px;  /* Preferred icon size */
+      display: inline-block;
+      line-height: 1;
+      text-transform: none;
+      letter-spacing: normal;
+      word-wrap: normal;
+      white-space: nowrap;
+      direction: ltr;
+    
+      /* Support for all WebKit browsers. */
+      -webkit-font-smoothing: antialiased;
+      /* Support for Safari and Chrome. */
+      text-rendering: optimizeLegibility;
+    
+      /* Support for Firefox. */
+      -moz-osx-font-smoothing: grayscale;
+    
+      /* Support for IE. */
+      font-feature-settings: 'liga';
+    }`],
   template: '<ng-content></ng-content>'
 })
 export class IconComponent { }

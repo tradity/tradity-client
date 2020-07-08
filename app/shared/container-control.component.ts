0500e9fab9c9a579363dc37f29751cdaa047a21e
Nico Haesler <nico@entless.org>
2020-07-08 15:47:26 +0200

    inline component css

--- app/shared/container-control.component.ts
+++ app/shared/container-control.component.ts
@@ -5,7 +5,33 @@ import { ContainerComponent } from './container.component';
 @Component({
   selector: 'tradity-container-control',
   template: '<ng-content></ng-content>',
-  styleUrls: ['container-control.component.css']
+  styles: [`
+    :host {
+      display: flex;
+      justify-content: space-between;
+      border: 1px solid #170804;
+      border-radius: 40px;
+      padding: 12px;
+      font-size: 12px;
+      font-weight: bold;
+      line-height: 15px;
+      color: #170804;
+    }
+    
+    :host.active,
+    :host:hover {
+      border-color: #F1592A;
+    }
+    
+    :host::after {
+      font-family: "Material Icons";
+      font-size: 24px;
+      content: "expand_more";
+    }
+    
+    :host.active::after {
+      content: "expand_less";
+    }`]
 })
 export class ContainerControlComponent {
   @HostBinding('class.active') isActive: boolean = false;

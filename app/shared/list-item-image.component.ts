0f915b63f50a93a3f3276f18f5e1056a733d5bf7
Nico Haesler <nico@entless.org>
2020-07-08 15:49:14 +0200

    inline component css

--- app/shared/list-item-image.component.ts
+++ app/shared/list-item-image.component.ts
@@ -2,7 +2,31 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'tradity-li-img',
-  styleUrls: ['list-item-image.component.css'],
+  styles: [`
+    :host {
+      position: absolute;
+      width: 50px;
+      height: 50px;
+      left: -60px;
+      border-radius: 50%;
+      margin-top: 5px;
+      background-color: rgba(23,8,4,0.02);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    
+    img {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+    }
+    
+    span {
+      font-weight: bold;
+      font-size: 25px;
+      line-height: 25px;
+    }`],
   template: '<img *ngIf="src" [src]="src" /><span *ngIf="!src">{{ placeholder.toUpperCase()[0] }}</span>'
 })
 export class ListItemImageComponent {

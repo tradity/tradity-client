f3ec8e68fbdd64b399565e150b1cf20258655b96
Nico Haesler <nico@entless.org>
2020-07-08 15:48:42 +0200

    inline component css

--- app/shared/list-item-action-list.component.ts
+++ app/shared/list-item-action-list.component.ts
@@ -3,6 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tradity-li-action-list',
   template: '<ng-content></ng-content>',
-  styleUrls: ['list-item-action-list.component.css']
+  styles: [`
+    :host {
+      display: flex;
+      flex-basis: 100%;
+      justify-content: space-around;
+      margin: -5px 0 5px;
+    }
+    
+    :host >>> a {
+      text-decoration: none;
+      color: rgba(23,8,4,0.8);
+    }`]
 })
 export class ListItemActionListComponent { }

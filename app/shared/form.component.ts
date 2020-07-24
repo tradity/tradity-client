d9a8269a1df562aa8723798d70bc0d3bb6ef7370
Nico Haesler <nico@entless.org>
2020-07-25 01:51:33 +0200

    remove checkbox styles from form component

--- app/shared/form.component.ts
+++ app/shared/form.component.ts
@@ -33,37 +33,6 @@ import { Component, OnInit } from '@angular/core';
         -webkit-text-fill-color: #170804;
     }
     
-    :host >>> input[type=checkbox] {
-      position: absolute;
-      left: -10000px;
-    }
-    
-    :host >>> input[type=checkbox] + label {
-      display: inline-flex;
-      align-items: center;
-      font-size: 12px;
-      line-height: 24px;
-      margin-bottom: 15px;
-    }
-    
-    :host >>> input[type=checkbox] + label::before {
-      content: "check_box_outline_blank";
-      font-family: "Material Icons";
-      font-size: 32px;
-      color: #D8D8D8;
-      margin: 0 6px 0 -3px;
-    }
-    
-    :host >>> input[type=checkbox]:focus + label::before,
-    :host >>> input[type=checkbox]:hover + label::before {
-      color: #F1592A;
-    }
-    
-    :host >>> input[type=checkbox]:checked + label::before {
-      content: "check_box";
-      color: #F1592A;
-    }
-    
     :host >>> input[type=radio] {
       position: absolute;
       left: -10000px;

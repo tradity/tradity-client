0deb2543502c5c3eb7814cde13819916e7f966fe
Nico Haesler <nico@entless.org>
2020-07-25 12:52:14 +0200

    inline template and styles

--- app/stocks/stock-detail.component.ts
+++ app/stocks/stock-detail.component.ts
@@ -12,8 +12,45 @@ import { Stock } from './stock.model';
 
 @Component({
   selector: 'tradity-stock-detail',
-  templateUrl: 'stock-detail.component.html',
-  styleUrls: ['stock-detail.component.css']
+  template: `
+    <tradity-li-header class="big-letters">
+      <span left>{{stock.name}}</span>
+      <span left>{{stock.stocktextid}}</span>
+      <span right>{{stock.ask / 10000 | currency:'EUR'}}</span>
+    </tradity-li-header>
+    <tradity-li-vl>
+      <tradity-li-vi>
+        <span i18n>Bid price</span>
+        <span>{{stock.bid / 10000 | currency:'EUR'}}</span>
+      </tradity-li-vi>
+      <tradity-li-vi>
+        <span i18n>Tradable amount</span>
+        <span>{{stock.pieces}} pc</span>
+      </tradity-li-vi>
+    </tradity-li-vl>
+    <a [routerLink]="['/stocks/' + stock.stocktextid + '/trade']" i18n>Trade</a>
+  `,
+  styles: [`
+    :host {
+      display: flex;
+      flex-direction: column;
+    }
+    
+    a {
+      padding: 17px 117px;
+      margin: 30px 0 50px;
+      background-color: rgba(40,59,144,0.95);
+      box-shadow: 0 2px 1px 0 #283B90;
+      border: 1px solid rgba(40,59,144,0.97);
+      border-radius: 3px;
+      font-size: 16px;
+      font-weight: bold;
+      text-align: center;
+      line-height: 19px;
+      color: #FFFFFF;
+      text-decoration: none;
+    }
+  `]
 })
 export class StockDetailComponent implements OnDestroy {
   private stockSubscription: Subscription;

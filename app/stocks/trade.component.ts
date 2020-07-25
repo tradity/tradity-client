fd0544d2640eb0d849d3fc629eb97d99e70159e6
Nico Haesler <nico@entless.org>
2020-07-25 13:57:38 +0200

    inline template and styles

--- app/stocks/trade.component.ts
+++ app/stocks/trade.component.ts
@@ -16,8 +16,62 @@ import * as authActions from '../auth/auth.actions';
 
 @Component({
   selector: 'tradity-trade',
-  templateUrl: 'trade.component.html',
-  styleUrls: ['trade.component.css']
+  template: `
+    <tradity-li-header class="big-letters">
+      <span left>{{stock.name}}</span>
+      <span left>{{stock.stocktextid}}</span>
+      <span right>{{stock.pieces}} pc</span>
+    </tradity-li-header>
+    <form tradity-form (ngSubmit)="trade()" autocomplete="off">
+      <input type="radio" name="sellbuy" id="buy" [checked]="sellBuy === Buy ? 'checked' : null" (click)="inputSellBuy(Buy)" />
+      <label for="buy" i18n>Buy</label>
+      <input type="radio" name="sellbuy" id="sell" [checked]="sellBuy === Sell ? 'checked' : null" (click)="inputSellBuy(Sell)" />
+      <label for="sell" i18n>Sell</label>
+      <span id="cash" i18n>Cash available: {{user.freemoney / 10000 | currency:'EUR'}}</span>
+      <tradity-input type="number" id="amount" name="amount" [value]="amount | async" min="0" [max]="stock.pieces" placeholder="Number of shares" i18n-placeholder (input)="inputAmount($event.target.value)"></tradity-input>
+      <span>x <span [class.inactive]="sellBuy === Sell">{{stock.ask / 10000 | currency:'EUR'}} (Ask)</span> | <span [class.inactive]="sellBuy === Buy">{{stock.bid / 10000 | currency:'EUR'}} (Bid)</span> =</span>
+      <tradity-input type="number" step="any" id="value" name="value" [value]="value | async" placeholder="Euros to be invested" i18n-placeholder (input)="inputValue($event.target.value)"></tradity-input>
+      <span>+ {{fee | async | currency:'EUR'}} transaction fee</span>
+      <button tradity-button type="submit" i18n>Execute trade</button>
+    </form>
+  `,
+  styles: [`
+    [tradity-form] {
+      margin: 0 50px;
+    }
+    
+    [tradity-form] tradity-input#amount + span {
+      display: block;
+      text-align: center;
+      margin: 0 auto 1rem;
+      font-size: 15px;
+      line-height: 19px;
+      letter-spacing: 1.5px;
+      color: #170804;
+    }
+    
+    [tradity-form] tradity-input#amount + span span.inactive {
+      color: rgba(23,8,4,0.2);
+    }
+    
+    [tradity-form] tradity-input#value + span {
+      margin: -6px auto 20px;
+      text-align: center;
+      font-size: 10px;
+      line-height: 12px;
+      letter-spacing: 1px;
+      color: #170804;
+    }
+    
+    [tradity-form] span#cash {
+      text-align: center;
+      margin: 0 auto 20px;
+      font-size: 15px;
+      line-height: 19px;
+      letter-spacing: 1.5px;
+      color: #170804;
+    }
+  `]
 })
 export class TradeComponent implements OnDestroy {
   private stockSubscription: Subscription;
